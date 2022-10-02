5bb0a0bc008f35ec90924696a55725b86f5a77de
Facundovvv <facundovillanovacolon@gmail.com>
2022-10-01 23:18:51 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//variables
+const powerbutton = document.querySelector('#powerbutton');
+const submitButton = document.querySelector('#botonDeSubmit');
+const pokemonTitle = document.querySelector('#pokemon-title');
+
